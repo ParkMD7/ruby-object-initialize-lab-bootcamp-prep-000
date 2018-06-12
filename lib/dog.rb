8ba79379e2042764ec694235eb3cd3738205fcaf
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-06-12 19:45:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,24 @@
 class Dog
   
-  def initialize(name, breed = "Mutt")
+  def initialize(name, breed)
     @name = name
-    
+    @breed = "Mutt"
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end   
